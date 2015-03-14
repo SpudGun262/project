b0a054aff69d653d73601b287c72e4523ad1cedb
SpudGun262 <thomas.walker@go.edgehill.ac.uk>
2015-03-14 17:22:04 +0000

    Test Commit

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/project/';
+$config['base_url']	= 'http://localhost/project/dfgdsjfg';
 
 /*
 |--------------------------------------------------------------------------
