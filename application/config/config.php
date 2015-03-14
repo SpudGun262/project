412db54a07e8568d4d215625c3c61650528a39ae
SpudGun262 <thomas.walker@go.edgehill.ac.uk>
2015-03-14 17:23:01 +0000

    Reverting changes made in test commit

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/project/dfgdsjfg';
+$config['base_url']	= 'http://localhost/project/';
 
 /*
 |--------------------------------------------------------------------------
