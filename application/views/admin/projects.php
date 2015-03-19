1c3cc1a14498ae6377aaa8eb46e52c5a21845a3b
SpudGun262 <thomas.walker@go.edgehill.ac.uk>
2015-03-19 17:29:24 +0000

    Coverted foreach loop so that it produces results in a HTML table

--- application/views/admin/projects.php
+++ application/views/admin/projects.php
@@ -3,14 +3,29 @@
 <a href="<?=base_url('admin/addProject')?>">&plus; Add New</a>
 
 <div class="adminProjectsTable">
-    <?php
-    foreach ($projects as $project) {
-        echo '<p>' . $project['title'] . '</p>';
-        echo '<p>' . $project['name'] . '</p>';
-        //reorder date to English standard (http://php.net/strtotime, http://php.net/manual/en/function.date.php)
-        ////uses HTML5 time tag (http://html5doctor.com/the-time-element/)
-        echo '<time datetime="' . $project['date_added'] . '">' . date('d-m-Y', strtotime($project['date_added'])) . '</time>';
-        echo '<a href="' . base_url('admin/editProject') . '">Edit</a>';
-    }
-    ?>
+    <table >
+        <tr>
+            <td>
+                Project Title
+            </td>
+            <td>
+               Course
+            </td>
+            <td>
+                Data Added
+            </td>
+        </tr>
+        <?php
+        foreach ($projects as $project) {
+            echo '<tr>';
+                echo '<td>' . $project['title'] . '</td>';
+                echo '<td>' . $project['name'] . '</td>';
+                //reorder date to English standard (http://php.net/strtotime, http://php.net/manual/en/function.date.php)
+                ////uses HTML5 time tag (http://html5doctor.com/the-time-element/)
+                echo '<td><time datetime="' . $project['date_added'] . '">' . date('d-m-Y', strtotime($project['date_added'])) . '</time></td>';
+                echo '<td><a href="' . base_url('admin/editProject') . '">Edit</a></td>';
+            echo '</tr>';
+        }
+        ?>
+    </table>
 </div>
