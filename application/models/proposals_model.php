720fbea4a89154d5765558a0bb6ecfa2a181de37
Tom Walker <thomas.walker@go.edgehill.ac.uk>
2015-03-25 16:13:30 +0000

    Implemented get_proposals method. Got data to render to the view.

--- application/models/proposals_model.php
+++ application/models/proposals_model.php
@@ -9,8 +9,22 @@ class Proposals_model extends CI_Model
         $this->load->database();
     }
 
+    public function proposal_base() {
+        //select all from the proposal table and join the courses, files, and tutors associated with the project
+        $this->db->select('
+        proposal.proposal_id, proposal.title, proposal.desc, proposal.date_added, proposal.tutor_id, proposal.course_id,
+        course.name,
+        tutor.first_name, tutor.last_name, tutor.email
+        ');
+        $this->db->from('proposal');
+        $this->db->join('course', 'course.course_id = proposal.course_id', 'left');
+        $this->db->join('tutor', 'tutor.tutor_id = proposal.tutor_id', 'left');
+        $this->db->order_by('proposal.date_added', 'desc');
+
+    }
+
     public function get_proposals() {
-        $query = $this->db->get('proposal');
-        return $query;
+        $this->proposal_base();
+        return $this->db->get();
     }
 }
---
+++ application/views/admin/proposals.php
@@ -0,0 +1,46 @@
+<h1>This is the admin proposals page</h1>
+
+<?=
+//TODO: Style flash data
+$this->session->flashdata('error', '<div class="error">', '</div>');?>
+
+<?=$this->session->flashdata('notice', '<div class="notice">', '</div>');?>
+
+<div class="adminProposalsTable column">
+
+    <!--When clicked run the addproposal method in the proposals controller-->
+    <a href="<?=base_url('admin/proposals/addProposal')?>" class="secondary button">&plus; Add New</a>
+
+    <table >
+        <thead>
+        <tr>
+            <th>Proposal Title</th>
+            <th>Course</th>
+            <th>Associated Tutor</th>
+            <th>Data Added</th>
+            <th>Edit proposal</th>
+            <th>Delete proposal</th>
+        </tr>
+        </thead>
+        <?php
+        //for each of the proposals, echo them as a single proposal inside a HTML table
+        foreach ($proposals as $proposal) {
+            echo '<tr>';
+            //Echo the title of the proposal
+            echo '<td>' . $proposal['title'] . '</td>';
+            //echo the name of the course the proposal is associated with
+            echo '<td>' . $proposal['name'] . '</td>';
+
+            //echo the first and last name of the tutor who uploaded the proposal
+                echo '<td>' . $proposal['first_name'] . ' ' . $proposal['last_name'] . '</td>';
+            //reorder date to English standard (http://php.net/strtotime, http://php.net/manual/en/function.date.php)
+            ////uses HTML5 time tag (http://html5doctor.com/the-time-element/)
+            echo '<td><time datetime="' . $proposal['date_added'] . '">' . date('d-m-Y', strtotime($proposal['date_added'])) . '</time></td>';
+            //echo an edit button
+            echo '<td><a href="' . base_url('admin/proposals/editproposal') .'/' . $proposal['proposal_id'] . '" class="radius button small">Edit</a></td>';
+            echo '<td><a href="' . base_url('admin/proposals/deleteproposal') .'/' . $proposal['proposal_id'] . '" class="alert radius button small">Delete</a></td>';
+            echo '</tr>';
+        }
+        ?>
+    </table>
+</div>
