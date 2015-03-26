66bb847308b90f46c551a0df494bc3e539d4fd00
Tom Walker <thomas.walker@go.edgehill.ac.uk>
2015-03-26 11:58:02 +0000

    Created addProposal view

--- application/controllers/admin/proposals.php
+++ application/controllers/admin/proposals.php
@@ -27,4 +27,52 @@ class Proposals extends CI_Controller
         $this->load->view('admin/proposals', $data);
         $this->load->view('admin/incs/footer');
     }
+
+    public function addProposal() {
+
+        //The form validation rules are set. The proposal title and abstract are required for the validation to pass. It must also be clear of any code
+        $this->form_validation->set_rules('proposal_title', 'Title', 'required|max_length[200]|xss_clean');
+        $this->form_validation->set_rules('abstract', 'Abstract', 'required|max_length[200]|xss_clean');
+
+        //File upload conditions
+        $config['upload_path'] = './uploads/';
+        $config['allowed_types'] = 'gif|jpg|png|pdf';
+        $config['max_size']	= '100';
+        $config['max_width']  = '0';
+        $config['max_height']  = '0';
+        $config['remove_spaces'] = TRUE;
+        $config['overwrite'] = TRUE;
+        $this->load->library('upload', $config);
+        $this->upload->do_upload();
+
+        $data['file'] = true;
+
+        if($this->input->post()) {
+            if ($_FILES['userfile']['error'] != UPLOAD_ERR_NO_FILE) {
+                if ($this->upload->display_errors() !== '') {
+                    $data['file'] = false;
+                }
+            }
+        }
+        //if the form passes validation then...
+        if($this->form_validation->run() == true && $data['file']){
+
+            //run the add_proposal method in the proposals_model
+            $this->proposals_model->add_proposal();
+
+            //redirect back to the proposals page
+            redirect('admin/proposals');
+
+            //if the form fails validation then...
+        } else {
+
+            //gather the data from the database again and load the proposals view
+            //Any validation errors will be displayed on the page. This is done by validation_errors() method in the addproposal view
+            $data['courses'] =  $this->courses_model->get_courses()->result_array();
+            $this->load->view('admin/incs/header');
+            $this->load->view('admin/addproposal', $data);
+            $this->load->view('admin/incs/footer');
+
+        }
+    }
 }
---
+++ application/views/admin/addProposal.php
@@ -0,0 +1,49 @@
+<h1>This is the add proposal page</h1>
+
+<?php
+//echo the validation errors if there is any
+echo validation_errors('<div class="error">', '</div>');
+
+echo '<div class="columns">';
+
+//open the HTML form
+echo form_open_multipart('admin/proposals/addProposal');
+
+//values for the proposal title input box
+$input = array(
+    'name' => 'proposal_title'
+);
+//echo a HTML label and input
+echo form_label('Title', 'proposal_title').form_input($input);
+
+//values for the abstract input box
+$input = array(
+    'name' => 'desc'
+);
+//echo a HTML label and input
+echo form_label('Description', 'desc').form_input($input);
+
+//echo a HTML select box
+echo '<select name="course">';
+//for each of the proposals, echo the available courses as a option
+foreach ($courses as $course) {
+    echo '<option value="' . $course['course_id'] . '">' . $course['name'] . '</option>';
+}
+//Close the select box
+echo '</select>';
+
+//values for the submit button
+$button = array(
+    'name' => 'add_proposal',
+    'value' => 'Add New proposal',
+    'class' => 'button radius'
+);
+//echo a submit button
+echo form_submit($button);
+
+//close the HTML form
+echo form_close();
+
+echo '</div>';
+
+?>
