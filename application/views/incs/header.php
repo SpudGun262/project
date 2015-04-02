afa772ea6553e63119db8054b5ecc807e95a16c5
Tom Walker <thomas.walker@go.edgehill.ac.uk>
2015-04-02 15:50:57 +0100

    Added time of day based message using PHPs date function. Depending at what time of day the user logs in, depends on the message they get

--- application/views/incs/header.php
+++ application/views/incs/header.php
@@ -32,9 +32,28 @@ if(!$CI->session->userdata('user_auth')) {
 
 } else {
 
-    echo '<p>Welcome ' . $this->session->userdata('user_auth')['first_name'] . '</p>';
+    $time = date('H:i:s');
 
-    echo '<p><a href="' . base_url('login/logout') . '" id="logout">Logout</a></p>';
+    echo $time;
+
+    if($time >= '06:00:00' && $time <= '11:59:59') {
+        echo '<p>Morning ' . $this->session->userdata('user_auth')['first_name'] . '</p>';
+
+    } elseif($time > '12:00:00' && $time <= '17:59:59') {
+
+        echo '<p>Afternoon ' . $this->session->userdata('user_auth')['first_name'] . '</p>';
+
+    } elseif($time > '18:00:00' && $time <= '23:59:59'){
+
+        echo '<p>Evening ' . $this->session->userdata('user_auth')['first_name'] . '</p>';
+
+    } elseif($time > '00:00:00' && $time <= '05:59:59'){
+
+        echo '<p>Wow you\'re up late! Or is this early for you ' . $this->session->userdata('user_auth')['first_name'] . '?</p>';
+
+    }
+
+    echo '<p><a href="' . base_url('login/logout') . '" id="logout"><i class="fa fa-sign-out fa-fw"></i>&nbsp;Logout</a></p>';
 
 }
 ?>
