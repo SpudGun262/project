a5d7b5baf0ea0034692d7602648deb9ed2b6a652
Tom Walker <thomas.walker@go.edgehill.ac.uk>
2015-04-03 17:36:12 +0100

    Implemented datatables. Used on the projects view

--- application/views/projects.php
+++ application/views/projects.php
@@ -1,8 +1,18 @@
+
+<script type="text/javascript" class="init">
+    $(document).ready(function(){
+        $('#projectsTable').DataTable({
+            "bFilter": false
+        });
+    });
+</script>
+
+
 <h1>Project Page</h1>
 
 <div class="row">
 
-    <table>
+    <table id="projectsTable" class="display">
         <thead>
         <tr>
             <th>Project Title</th>
@@ -13,21 +23,22 @@
         </thead>
         <tbody>
 
+
             <?php
 
             foreach ($projects as $project) {
                 echo '<tr>';
-                    echo '<td>' . $project['title'] . '</td>';
+                    echo '<td><a href="' . base_url('projects/viewProject/') . '/' . $project['project_id'] . '">' . $project['title'] . '</a></td>';
                     echo '<td>' . maxLength($project['abstract']) . '...</td>';
                     echo '<td>' . $project['course_name'] . '</td>';
                     //reorder date to English standard (http://php.net/strtotime, http://php.net/manual/en/function.date.php)
                     //uses HTML5 time tag (http://html5doctor.com/the-time-element/)
                     echo '<td><time datetime="' . $project['date_added'] . '">' . date('d-m-Y', strtotime($project['date_added'])) . '</time></td>';
-                echo '<tr>';
+                echo '</tr>';
             }
             ?>
+
         </tbody>
     </table>
 
 </div>
-
