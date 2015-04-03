e505806180bd614594f2462ecbb2d6ca76ae8e6d
SpudGun262 <thomas.walker@go.edgehill.ac.uk>
2015-04-03 16:30:42 +0100

    Created a maxLength helper. It contains helper function that cuts down a strign that is over 100 characters. Implemented it in the projects view

--- application/views/projects.php
+++ application/views/projects.php
@@ -1,28 +1,33 @@
 <h1>Project Page</h1>
 
+<div class="row">
 
-<table>
-    <thead>
-    <tr>
-        <th>Project Title</th>
-        <th>Short Description</th>
-        <th>Course</th>
-        <th>Date Added</th>
-    </tr>
-    </thead>
+    <table>
+        <thead>
+        <tr>
+            <th>Project Title</th>
+            <th>Short Description</th>
+            <th>Course</th>
+            <th>Date Added</th>
+        </tr>
+        </thead>
+        <tbody>
 
-    <?php
+            <?php
 
-    foreach ($projects as $project) {
-        echo '<tr>';
-        echo '<td>' . $project['title'] . '</td>';
-        echo '<td>' . maxLength($project['abstract']) . '...</td>';
-        echo '<td>' . $project['course_name'] . '</td>';
-        //reorder date to English standard (http://php.net/strtotime, http://php.net/manual/en/function.date.php)
-        //uses HTML5 time tag (http://html5doctor.com/the-time-element/)
-        echo '<td><time datetime="' . $project['date_added'] . '">' . date('d-m-Y',
-                strtotime($project['date_added'])) . '</time></td>';
-        echo '<tr>';
-    }
-    ?>
-</table>
+            foreach ($projects as $project) {
+                echo '<tr>';
+                    echo '<td>' . $project['title'] . '</td>';
+                    echo '<td>' . maxLength($project['abstract']) . '...</td>';
+                    echo '<td>' . $project['course_name'] . '</td>';
+                    //reorder date to English standard (http://php.net/strtotime, http://php.net/manual/en/function.date.php)
+                    //uses HTML5 time tag (http://html5doctor.com/the-time-element/)
+                    echo '<td><time datetime="' . $project['date_added'] . '">' . date('d-m-Y', strtotime($project['date_added'])) . '</time></td>';
+                echo '<tr>';
+            }
+            ?>
+        </tbody>
+    </table>
+
+</div>
+
