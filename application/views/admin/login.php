74d154386d91e10ef3f7658974c92eb1c8856148
Tom Walker <thomas.walker@go.edgehill.ac.uk>
2015-04-07 00:17:14 +0100

    Cleaned up the admin login page. Still needs foundation functionality adding to it

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -1,17 +1,22 @@
-<?php
+<div class="row">
+    <h1>Admin Login</h1>
 
-echo '<h1>Login</h1>';
+    <?php
 
-if(isset($error)) {
-    echo '<div>'.$error.'</div>';
-}
+    if(isset($error)) {
+        echo '<div>'.$error.'</div>';
+    }
 
-echo form_open();
-echo validation_errors();
-echo form_input('username', set_value('username'), 'placeholder="Username"');
-echo form_password('password', '', 'placeholder="Password" class="password"');
-echo form_submit('submit', 'Login', 'class="radius button"');
-echo form_close();
+    echo form_open();
+    echo validation_errors();
+    echo form_input('username', set_value('username'), 'placeholder="Username"');
+    echo form_password('password', '', 'placeholder="Password" class="password"');
+    echo form_submit('submit', 'Login', 'class="radius button"');
+    echo form_close();
 
-//Add this link to improve navigation
-echo '<a href="' . base_url() . '">Back to home page</a>';
+    //Add this link to improve navigation
+    echo '<a href="' . base_url() . '">Back to home page</a>';
+
+    ?>
+
+</div>
