57083f95b4dfdb63a3e8afd19c49a169a0b46c70
SpudGun262 <thomas.walker@go.edgehill.ac.uk>
2015-04-13 14:40:49 +0100

    Fully commented all helper files

--- application/helpers/assets_helper.php
+++ application/helpers/assets_helper.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Asset URL Helper
+ *
+ * This function grants access to the assets folder
+ *
+ * @usage echo asset_url().'string';
+ * @return string
+ */
 function asset_url(){
     return base_url().'assets/';
 }
--- application/helpers/upload_helper.php
+++ application/helpers/upload_helper.php
@@ -1,5 +1,14 @@
 <?php
 
+
+/**
+ * Upload URL Helper
+ *
+ * This function grants access to the uploads folder
+ *
+ * @usage echo upload_url().'string';
+ * @return string
+ */
 function upload_url(){
     return base_url().'uploads/';
 }
